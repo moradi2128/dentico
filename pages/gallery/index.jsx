b4ba16a31a2caf8b028a28d8e3148780fd13c3e5
mohammadreza moradi <mroadi2565@gmail.com>
2023-04-16 23:22:36 +0430

    [fix] fix featuredImage in time is null

--- pages/gallery/index.jsx
+++ pages/gallery/index.jsx
@@ -10,7 +10,7 @@ const GalleryItem = ({ title, destination, date, imageUri, iamgeAlt }) => {
   return <Link href={destination}>
     <a>
       <div className={`w-full group transition-all relative mb-6 overflow-hidden card-rounded min-h-[500px] shadow-custom-lg hover:shadow-black/40 border border-gray-200`}>
-        <Image src={imageUri} layout="fill" objectFit='cover' alt={iamgeAlt || ""} className="group-hover:scale-105 transition-all duration-300" />
+        {imageUri && <Image src={imageUri} layout="fill" objectFit='cover' alt={iamgeAlt || ""} className="group-hover:scale-105 transition-all duration-300" />}
         <div className=' absolute bottom-0 right-0 left-0 opacity-50 transition-all duration-300 h-[100%] group-hover:h-[100%]  bg-gradient-to-t from-primary group-hover:to-primary/60 ' />
         {/* === content === */}
         <div className='[&>p]:text-lg text-white absolute bottom-0 right-0 left-0 p-10 '>
@@ -31,14 +31,14 @@ const Gallery = (props) => {
   return (
     <div className='container mx-auto px-5'>
       <div className='columns-1 md:columns-2 lg:columns-3 gap-5 py-16'>
-        {(props.gallery || []).map((item) => {
+        {(props.gallery || []).map((item,i) => {
           return <GalleryItem
-            key={item.id}
+            key={i}
             title={item.title}
             destination={item.uri}
             date={item.date}
-            imageUri={item.featuredImage.node.sourceUrl}
-            iamgeAlt={item.featuredImage.node.altText}
+            imageUri={item.featuredImage?.node.sourceUrl}
+            iamgeAlt={item.featuredImage?.node.altText}
           />
         })}
       </div>
