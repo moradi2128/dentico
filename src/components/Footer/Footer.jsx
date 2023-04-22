89053b95424beff22acf9bd3fe1af9843fbdb04f
mohammadreza moradi <mroadi2565@gmail.com>
2023-04-22 22:08:06 +0430

    [Fix] fix bug in social media footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = (props) => {
       <div className="mx-auto max-w-screen-xl px-4 pt-36 pb-6 ">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 prose-li:list-none">
           {/* === Right Footer === */}
-          <div className='relative h-[300px]'>
-            <div className='bg-white h-[470px] absolute top-[-200px] card-rounded shadow-custom-lg shadow-secondary/20 p-8'>
+          <div className='relative min-h-[300px] '>
+            <div className='w-full bg-white min-h-[470px] absolute top-[-250px] card-rounded shadow-custom-lg shadow-secondary/20 p-8'>
               {/* === Logo === */}
               <Logo src={props.logo} />
               <div
-                className='prose prose-ul:flex prose-ul:gap-7'
+                className='prose prose-ul:flex prose-ul:gap-7 prose-ul:flex-wrap'
                 dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(props.footer.sidebarOne) }}
               />
             </div>
