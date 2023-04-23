4d972c544d4e11a8cfcccb258ad847bb153b677e
mohammadreza moradi <mroadi2565@gmail.com>
2023-04-23 10:03:18 +0430

    [Fix] fix bug in loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div class="fixed top-0 right-0 h-screen w-screen z-[60] flex justify-center items-center">
-            <div class="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+        <div className="fixed top-0 right-0 h-screen w-screen z-[60] flex justify-center items-center">
+            <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"/>
         </div>
     )
 }
